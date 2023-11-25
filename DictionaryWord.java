0f9c908e140bbe6b3197134a578430d58944b682
siyuan <siyuann.su@gmail.com>
2023-11-25 23:10:23 +0800

    First commit

---
+++ DictionaryWord.java
@@ -0,0 +1,12 @@
+public class DictionaryWord {
+    private String word;
+    private String translation;
+    private String example;
+    private String grammar;
+    private String IDIOM ;
+    private String PARADIGM ;
+    private String INFLECTION ;
+    private String EXPLANATION ;
+
+
+}
